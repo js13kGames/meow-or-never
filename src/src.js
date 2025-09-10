ea9770f12e4ee81a1845f1765126237a08db51e1
Markus Fisch <mf@markusfisch.de>
2025-09-10 23:49:30 +0200

    Add turns and swap cards

--- src/src.js
+++ src/src.js
@@ -1,38 +1,53 @@
 "use strict"
 
-const SLOTS = [-1.5, -0.5, 0.5, 1.5]
+const SLOTS = [-1.5, -0.5, 0.5, 1.5],
+	PLAYERS = []
 
-let zIndex = 999
+let zIndex = 999,
+	turn = 0,
+	topCard
+
+function alignCard(card, side, slot) {
+	card.className = "card " + (side || "")
+	card.style.setProperty('--i', SLOTS[slot || 0]);
+	card.game.side = side
+	card.game.slot = slot
+}
 
 function playCard(card) {
+	if (card.game.side != PLAYERS[turn]) {
+		return
+	}
+
 	card.onclick = function() { return false }
 	card.style.zIndex = ++zIndex
 
-	const cardTransform = card.style.transform
-	card.animate(
-		[
-			{transform: cardTransform},
-			{transform: `translate(-50%, -50%) rotateZ(${
-				Math.random() * 10 - 5
-				}deg) scale(2)`},
-		], {
-			duration: 200,
-			fill: "forwards",
-		}
-	).play()
+	requestAnimationFrame(() => {
+		card.style.transform = `translate(-50%, -50%) rotateZ(${
+			Math.random() * 10 - 5}deg) scale(2)`
+	})
 
-	const next = newCard(card.game.side, card.game.slot)
-	next.style.transform = cardTransform
-	document.body.appendChild(next)
-	next.animate(
-		[
-			{transform: `translate(-60vw, -60vh)`},
-			{transform: cardTransform},
-		], {
-			duration: 200,
-			fill: "forwards",
+	if (topCard != null && topCard.game.value > card.game.value) {
+		const backCard = topCard
+		alignCard(backCard, card.game.side, card.game.slot)
+		backCard.onclick = function() {
+			playCard(backCard)
+			return false
 		}
-	).play()
+		requestAnimationFrame(() => {
+			backCard.style.transform = ""
+		})
+	} else {
+		const next = newCard(card.game.side, card.game.slot)
+		document.body.appendChild(next)
+		next.style.transform = "translate(-60vw, -60vh)"
+		requestAnimationFrame(() => {
+			next.style.transform = ""
+		})
+	}
+
+	topCard = card
+	turn = ++turn % PLAYERS.length
 }
 
 function newCard(side, slot) {
@@ -45,16 +60,12 @@ function newCard(side, slot) {
 		return false
 	}
 
-	card.className = "card " + (side || "")
-	card.style.setProperty('--i', SLOTS[slot || 0]);
-
 	card.game = {
-		value: value,
-		side: side,
-		slot: slot,
+		value: value
 	}
-
 	card.innerHTML = value
+
+	alignCard(card, side, slot)
 
 	return card
 }
@@ -62,10 +73,11 @@ function newCard(side, slot) {
 function setup(nplayers) {
 	document.body.innerHTML = ""
 
-	const sides = ["top", "bottom", "left", "right"],
+	const sides = ["bottom", "left", "top", "right"],
 		cardsPerPlayer = SLOTS.length
-	for (let i = 0; i < nplayers; ++i) {
-		const side = sides[i]
+	for (let i = 0, step = nplayers == 2 ? 2 : 1; i < nplayers; ++i) {
+		const side = sides[i * step]
+		PLAYERS.push(side)
 		for (let j = 0; j < cardsPerPlayer; ++j) {
 			document.body.appendChild(newCard(side, j))
 		}
